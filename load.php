b300a6d3ec38bc42e1046920739ffd8b49b45558
Nicolas <nicolasfracicaimb@gmail.com>
2024-07-08 18:05:45 -0500

    server.php

--- load.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'index.php'
-
-$columns = ['Numero de medias',	'Tipo de medias',	'Marca de medias',	'Entrada',	'Salida','Precio de medias';
-    $table = "entradas";
-
-    $campo= %conn->real_escape_string$_POST['campo'];
-]
